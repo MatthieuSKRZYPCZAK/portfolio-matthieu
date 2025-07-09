8215c1491bc3631dc8e12c02489113fd99d25cd3
Matthieu <46696317+MatthieuSKRZYPCZAK@users.noreply.github.com>
2025-07-09 11:19:29 +0200

    Feat: construction

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,7 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import FalloutTheme from "./components/themes/Fallout/Fallout";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import NavBar from "./components/Navbar/Navbar";
-
-import Home from "./components/Home/Home";
-import About from "./components/About/About";
-import Skills from "./components/Skills/Skills";
-import Contact from "./components/Contact/Contact";
-import Project from "./components/Project/Project";
-import Error from "./components/404/404";
 
 import construct from './assets/img/construct.gif'
 import "./App.css";
